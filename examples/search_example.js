0ff8971f4e0579247dd65cec7cbca2b61d8fc91e
Charlotte Vermandel <charlotte@meilisearch.com>
2020-01-10 16:07:39 +0100

    Create a search example with the provided small movie dataset

---
+++ examples/search_example.js
@@ -0,0 +1,43 @@
+var Meili = require('../dist')
+const dataset = require('./small_movies.json')
+
+const config = {
+  host: 'http://127.0.0.1:7700',
+}
+
+var meili = new Meili(config)
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+const index = {
+  name: 'Movies',
+  uid: 'movies',
+}
+
+const addDataset = async () => {
+    let resp
+    resp = await meili.listIndexes()
+    if (resp.length === 0) {
+      resp = await meili.createIndex(index)
+    }
+    resp = await meili.Index(index.uid).getDocuments()
+    if (resp.length === 0) {
+      resp = await meili.Index('movies').addDocuments(dataset)
+      await sleep(1000); // This is to give time to MeiliSearch to index the dataset
+      // If you have no results it means it took more than 1 second to index.
+    }
+}
+
+(async () => {
+  await addDataset()
+
+  let resp
+  resp = await meili.Index('movies').search({
+    q: 'Avengers',
+  })
+  console.log({ resp, hits: resp.hits })
+
+})()
+
