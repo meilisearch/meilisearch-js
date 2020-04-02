74bbe6b50f2bbd6ca84400d81d8c99144a06fd5e
charlotte <cvermand@student.42.fr>
2020-04-02 19:55:32 +0200

    Improve tests and types

--- tests/meili.ts
+++ tests/meili.ts
@@ -1,5 +1,6 @@
-import Meili from '../src'
+import MeiliSearch from '../src'
 import * as Types from '../src/types'
+import { types } from '@babel/core'
 
 const config = {
   host: 'http://127.0.0.1:7700',
@@ -7,20 +8,17 @@ const config = {
 }
 
 // TODO: do test with two meili servers, one with api key one without
-const meili = new Meili(config)
+const client = new MeiliSearch(config)
 
 const clearAllIndexes = async () => {
-  const indexes = await meili
+  const indexes = await client
     .listIndexes()
-    .then((response: any) => {
-      return response.map((elem: any) => elem.uid)
-    })
-    .catch((err) => {
-      expect(err).toBe(null)
+    .then((response: Types.IndexResponse[]): string[] => {
+      return response.map((elem: Types.IndexResponse) => elem.uid)
     })
 
   for (const indexUid of indexes) {
-    await meili
+    await client
       .getIndex(indexUid)
       .deleteIndex()
       .catch((err) => {
@@ -28,51 +26,84 @@ const clearAllIndexes = async () => {
       })
   }
 
-  await expect(meili.listIndexes()).resolves.toHaveLength(0)
+  await expect(client.listIndexes()).resolves.toHaveLength(0)
 }
 
 test('connexion without API key', () => {
-  const meiliNoApi = new Meili({
+  const meiliNoApi = new MeiliSearch({
     host: 'http://127.0.0.1:7700',
   })
-  expect(meiliNoApi).toBeInstanceOf(Meili)
+  expect(meiliNoApi).toBeInstanceOf(MeiliSearch)
 })
 
 test('connexion with API key', () => {
-  const meiliApi = new Meili({
+  const meiliApi = new MeiliSearch({
     host: 'http://127.0.0.1:7700',
     apiKey: '123',
   })
-  expect(meiliApi).toBeInstanceOf(Meili)
+  expect(meiliApi).toBeInstanceOf(MeiliSearch)
 })
 
 test('get different keys', async () => {
-  await expect(meili.getKeys()).resolves.toHaveProperty('public')
+  await client.getKeys().then((response: Types.Keys) => {
+    expect(response).toHaveProperty('public', expect.any(String))
+    expect(response).toHaveProperty('private', expect.any(String))
+  })
 })
 
 test('health', async () => {
-  await expect(meili.setHealthy()).resolves.toBe('')
-  await expect(meili.isHealthy()).resolves.toBe(true)
-  await expect(meili.setUnhealthy()).resolves.toBe('')
-  await expect(meili.isHealthy()).rejects.toThrow()
-  await expect(meili.setHealthy()).resolves.toBe('')
-  await expect(meili.isHealthy()).resolves.toBe(true)
-  await expect(meili.changeHealthTo(false)).resolves.toBe('')
-  await expect(meili.isHealthy()).rejects.toThrow()
-  await expect(meili.setHealthy()).resolves.toBe('')
+  await expect(client.setHealthy()).resolves.toBe('')
+  await expect(client.isHealthy()).resolves.toBe(true)
+  await expect(client.setUnhealthy()).resolves.toBe('')
+  await expect(client.isHealthy()).rejects.toThrow()
+  await expect(client.setHealthy()).resolves.toBe('')
+  await expect(client.isHealthy()).resolves.toBe(true)
+  await expect(client.changeHealthTo(false)).resolves.toBe('')
+  await expect(client.isHealthy()).rejects.toThrow()
+  await expect(client.setHealthy()).resolves.toBe('')
 })
 
 test('system information', async () => {
-  await expect(meili.systemInformation()).resolves.toBeDefined()
-  await expect(meili.systemInformationPretty()).resolves.toBeDefined()
+  await client.sysInfo().then((response: Types.SysInfo) => {
+    expect(response).toHaveProperty('memoryUsage', expect.any(Number))
+    expect(response).toHaveProperty('processorUsage', expect.any(Array))
+    expect(response.global).toHaveProperty('totalMemory', expect.any(Number))
+    expect(response.global).toHaveProperty('usedMemory', expect.any(Number))
+    expect(response.global).toHaveProperty('totalSwap', expect.any(Number))
+    expect(response.global).toHaveProperty('usedSwap', expect.any(Number))
+    expect(response.global).toHaveProperty('inputData', expect.any(Number))
+    expect(response.global).toHaveProperty('outputData', expect.any(Number))
+    expect(response.process).toHaveProperty('memory', expect.any(Number))
+    expect(response.process).toHaveProperty('cpu', expect.any(Number))
+  })
+  await client.prettySysInfo().then((response: Types.SysInfoPretty) => {
+    expect(response).toHaveProperty('memoryUsage', expect.any(String))
+    expect(response).toHaveProperty('processorUsage', expect.any(Array))
+    expect(response.global).toHaveProperty('totalMemory', expect.any(String))
+    expect(response.global).toHaveProperty('usedMemory', expect.any(String))
+    expect(response.global).toHaveProperty('totalSwap', expect.any(String))
+    expect(response.global).toHaveProperty('usedSwap', expect.any(String))
+    expect(response.global).toHaveProperty('inputData', expect.any(String))
+    expect(response.global).toHaveProperty('outputData', expect.any(String))
+    expect(response.process).toHaveProperty('memory', expect.any(String))
+    expect(response.process).toHaveProperty('cpu', expect.any(String))
+  })
 })
 
 test('Version', async () => {
-  await expect(meili.version()).resolves.toBeDefined()
+  await client.version().then((response: Types.Version) => {
+    expect(response).toHaveProperty('commitSha', expect.any(String))
+    expect(response).toHaveProperty('buildDate', expect.any(String))
+    expect(response).toHaveProperty('pkgVersion', expect.any(String))
+  })
 })
 
 test('Database stats', async () => {
-  await expect(meili.databaseStats()).resolves.toBeDefined()
+  await client.stats().then((response: Types.Stats) => {
+    expect(response).toHaveProperty('databaseSize', expect.any(Number))
+    expect(response).toHaveProperty('lastUpdate', expect.any(String))
+    expect(response).toHaveProperty('indexes', expect.any(Object))
+  })
 })
 
 test('reset-start', async () => {
@@ -91,30 +122,23 @@ test('create-index-with-uid', async () => {
     uid: '',
   }
 
-  await expect(meili.listIndexes()).resolves.toHaveLength(0)
+  await expect(client.listIndexes()).resolves.toHaveLength(0)
 
-  await meili
-    .createIndex(index)
-    .then((response: Types.CreateIndexResponse) => {
-      expect(response.uid).toBe(index.uid)
-    })
-    .catch((err) => {
-      expect(err).toBe(null)
-    })
+  await client.createIndex(index).then((response: Types.IndexResponse) => {
+    expect(response.uid).toBe(index.uid)
+  })
 
-  await expect(meili.listIndexes()).resolves.toHaveLength(1)
-  await expect(meili.createIndex(index)).rejects.toThrow()
-  await expect(meili.listIndexes()).resolves.toHaveLength(1)
-  await expect(meili.createIndex(noUid)).rejects.toThrow()
-  await meili
+  await expect(client.listIndexes()).resolves.toHaveLength(1)
+  await expect(client.createIndex(index)).rejects.toThrow()
+  await expect(client.listIndexes()).resolves.toHaveLength(1)
+  await expect(client.createIndex(noUid)).rejects.toThrow()
+  await client
     .createIndex(indexIndentifier)
-    .then((response: Types.CreateIndexResponse) => {
+    .then((response: Types.IndexResponse) => {
       expect(response.uid).toBe(indexIndentifier.uid)
     })
-    .catch((err) => {
-      expect(err).toBe(null)
-    })
-  await expect(meili.listIndexes()).resolves.toHaveLength(2)
+
+  await expect(client.listIndexes()).resolves.toHaveLength(2)
   await clearAllIndexes()
 })
 
