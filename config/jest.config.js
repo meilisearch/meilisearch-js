cbbd6ad04797d0b87007c2382d8d65a1cca4882d
charlotte <cvermand@student.42.fr>
2020-04-03 19:59:25 +0200

    Added env variables to dot env for the moment, will add soon to github actions and only require dotenv on developement env

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,9 +1,4 @@
-process.env.HOST = 'http://127.0.0.1:7700'
-process.env.MASTER_KEY = '123'
-process.env.PRIVATE_KEY =
-  '8c222193c4dff5a19689d637416820bc623375f2ad4c31a2e3a76e8f4c70440d'
-process.env.PUBLIC_KEY =
-  '948413b6667024a0704c2023916c21eaf0a13485a586c43e4d2df520852a4fb8'
+require('dotenv').config() // TODO: add env to github actions
 
 const config = {
   preset: 'ts-jest',
