ff155d10b4aef001ff6136f1b6034458412ef5d0
Charlotte Vermandel <charlotte@meilisearch.com>
2020-04-27 17:42:45 +0200

    Removed dotenv

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,3 @@
-require('dotenv').config()
-
 const config = {
   preset: 'ts-jest',
   rootDir: '.',
--- tests/meilisearch-test-utils.ts
+++ tests/meilisearch-test-utils.ts
@@ -1,26 +1,32 @@
 import MeiliSearch from '../src/meilisearch'
 import * as Types from '../src/types'
 
-const { HOST: host, MASTER_KEY, PRIVATE_KEY, PUBLIC_KEY } = process.env
+// testing
+const MASTER_KEY = 'masterKey'
+const HOST = 'http://127.0.0.1:7700'
+const PRIVATE_KEY =
+  '8dcbb482663333d0280fa9fedf0e0c16d52185cb67db494ce4cd34da32ce2092'
+const PUBLIC_KEY =
+  '3b3bf839485f90453acc6159ba18fbed673ca88523093def11a9b4f4320e44a5'
 
 const config = {
-  host: host ? host : 'no-host-provided',
+  host: HOST,
   apiKey: MASTER_KEY,
 }
 const masterClient = new MeiliSearch({
-  host: host ? host : 'no-host-provided',
+  host: HOST,
   apiKey: MASTER_KEY,
 })
 const privateClient = new MeiliSearch({
-  host: host ? host : 'no-host-provided',
+  host: HOST,
   apiKey: PRIVATE_KEY,
 })
 const publicClient = new MeiliSearch({
-  host: host ? host : 'no-host-provided',
+  host: HOST,
   apiKey: PUBLIC_KEY,
 })
 const anonymousClient = new MeiliSearch({
-  host: host ? host : 'no-host-provided',
+  host: HOST,
 })
 
 const clearAllIndexes = async (config: Types.Config) => {
