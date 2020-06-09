165edea57b18c851b7d9a25cd482808b3f1cbd30
Charlotte Vermandel <charlotte@meilisearch.com>
2020-06-09 10:09:39 +0200

    Typo in class name

--- src/errors/http-error-handler.ts
+++ src/errors/http-error-handler.ts
@@ -1,12 +1,12 @@
 import { AxiosError } from 'axios'
 import MeiliSearchApiError from './meilisearch-api-error'
-import MeilISearchCommunicationError from './meilisearch-communication-error'
+import MeiliSearchCommunicationError from './meilisearch-communication-error'
 
 function httpErrorHandler(e: AxiosError, cachedStack?: string): void {
   if (e.response !== undefined) {
     throw new MeiliSearchApiError(e, cachedStack)
   } else if (e.isAxiosError) {
-    throw new MeilISearchCommunicationError(e.message)
+    throw new MeiliSearchCommunicationError(e.message)
   } else {
     throw e
   }
