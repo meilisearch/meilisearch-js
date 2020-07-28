a27b99a5837a01d0f6316f5c28d48d83af863f31
Charlotte Vermandel <charlottevermandel@gmail.com>
2020-07-28 19:31:08 +0200

    Remove undefined from object before sending search

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,14 @@
+function removeUndefinedFromObject(object: Record<string, any>): object {
+  return Object.keys(object).reduce((acc: Record<string, any>, key: string) => {
+    if (object[key] !== undefined) {
+      acc[key] = object[key]
+    }
+    return acc
+  }, {})
+}
+
 async function sleep(ms: number): Promise<void> {
   return await new Promise((resolve) => setTimeout(resolve, ms))
 }
 
-export { sleep }
+export { sleep, removeUndefinedFromObject }
