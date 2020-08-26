49bc1d228afdf93bf4ea2fc7f65904d0e84a883a
cvermand <33010418+bidoubiwa@users.noreply.github.com>
2020-08-26 12:21:27 +0200

    Update index tests (#563)
    * Update index tests
    
    * Update tests/index_tests.ts
    
    Co-authored-by: Samuel Jimenez <sjimenezre@gmail.com>
    
    Co-authored-by: Samuel Jimenez <sjimenezre@gmail.com>

--- tests/index_tests.ts
+++ tests/index_tests.ts
@@ -113,8 +113,8 @@ describe.each([
       await expect(
         index.updateIndex({ primaryKey: 'newPrimaryKey' })
       ).rejects.toThrowError(
-        `The schema already have an primary key. It's impossible to update it`
-      ) // see issue in meilisearch/meilisearch
+        `A primary key is already present. It's impossible to update it`
+      )
     })
 
     test(`${permission} key: delete index`, async () => {
