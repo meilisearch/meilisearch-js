9ef160e96f73874688fdc062e7284addf5af4bf8
Charlotte Vermandel <charlottevermandel@gmail.com>
2020-12-07 11:20:06 +0100

    Placde timeout in setup file

--- tests/env/express/tests/client.test.js
+++ tests/env/express/tests/client.test.js
@@ -3,12 +3,10 @@ describe('MeiliSearch JS Browser test', () => {
     await page.goto('http://localhost:3000')
   })
 
-  jest.setTimeout(100 * 1000)
-
   it('Should have generated a meilisearch client and displayed', async () => {
     await page.waitForSelector("#indexes")
     await expect(
       page.content()
       ).resolves.toMatch('createdIndexTest')
-  }, 10000)
+  }, 30000)
 })
