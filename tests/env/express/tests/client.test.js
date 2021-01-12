a0d06c68e18c1d415b1e30682ef9afb8cd46b4f5
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-01-12 11:28:44 +0100

    Fixes tests on ubuntu

--- tests/env/express/tests/client.test.js
+++ tests/env/express/tests/client.test.js
@@ -4,6 +4,7 @@ describe('MeiliSearch JS Browser test', () => {
   })
 
   it('Should have generated a meilisearch client and displayed', async () => {
+    await page.waitForNavigation({waitUntil: "domcontentloaded"})
     await page.waitForSelector("#indexes")
     await expect(
       page.content()
