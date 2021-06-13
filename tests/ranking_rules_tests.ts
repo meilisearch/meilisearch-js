f78847db4898334fc8d33c1cff232f2878a22200
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-06-13 17:26:05 +0200

    Refactor ranking rules tests

--- tests/ranking_rules_tests.ts
+++ tests/ranking_rules_tests.ts
@@ -47,7 +47,7 @@ describe.each([
   { client: masterClient, permission: 'Master' },
   { client: privateClient, permission: 'Private' },
 ])('Test on ranking rules', ({ client, permission }) => {
-  beforeAll(async () => {
+  beforeEach(async () => {
     await clearAllIndexes(config)
     await masterClient.createIndex(index.uid)
     const { updateId } = await masterClient
@@ -55,6 +55,7 @@ describe.each([
       .addDocuments(dataset)
     await client.index(index.uid).waitForPendingUpdate(updateId)
   })
+
   test(`${permission} key: Get default ranking rules`, async () => {
     await client
       .index(index.uid)
@@ -63,6 +64,7 @@ describe.each([
         expect(response).toEqual(defaultRankingRules)
       })
   })
+
   test(`${permission} key: Update ranking rules`, async () => {
     const newRankingRules = ['asc(title)', 'typo', 'desc(description)']
     const { updateId } = await client
@@ -80,6 +82,7 @@ describe.each([
         expect(response).toEqual(newRankingRules)
       })
   })
+
   test(`${permission} key: Update ranking rules at null`, async () => {
     const { updateId } = await client
       .index(index.uid)
@@ -96,6 +99,7 @@ describe.each([
         expect(response).toEqual(defaultRankingRules)
       })
   })
+
   test(`${permission} key: Reset ranking rules`, async () => {
     const { updateId } = await client
       .index(index.uid)
@@ -117,20 +121,22 @@ describe.each([
 describe.each([{ client: publicClient, permission: 'Public' }])(
   'Test on ranking rules',
   ({ client, permission }) => {
-    beforeAll(async () => {
+    beforeEach(async () => {
       await clearAllIndexes(config)
-      await masterClient.createIndex(index.uid)
     })
+
     test(`${permission} key: try to get ranking rules and be denied`, async () => {
       await expect(
         client.index(index.uid).getRankingRules()
       ).rejects.toHaveProperty('errorCode', Types.ErrorStatusCode.INVALID_TOKEN)
     })
+
     test(`${permission} key: try to update ranking rules and be denied`, async () => {
       await expect(
         client.index(index.uid).updateRankingRules([])
       ).rejects.toHaveProperty('errorCode', Types.ErrorStatusCode.INVALID_TOKEN)
     })
+
     test(`${permission} key: try to reset ranking rules and be denied`, async () => {
       await expect(
         client.index(index.uid).resetRankingRules()
@@ -142,10 +148,10 @@ describe.each([{ client: publicClient, permission: 'Public' }])(
 describe.each([{ client: anonymousClient, permission: 'No' }])(
   'Test on ranking rules',
   ({ client, permission }) => {
-    beforeAll(async () => {
+    beforeEach(async () => {
       await clearAllIndexes(config)
-      await masterClient.createIndex(index.uid)
     })
+
     test(`${permission} key: try to get ranking rules and be denied`, async () => {
       await expect(
         client.index(index.uid).getRankingRules()
@@ -154,6 +160,7 @@ describe.each([{ client: anonymousClient, permission: 'No' }])(
         Types.ErrorStatusCode.MISSING_AUTHORIZATION_HEADER
       )
     })
+
     test(`${permission} key: try to update ranking rules and be denied`, async () => {
       await expect(
         client.index(index.uid).updateRankingRules([])
@@ -162,6 +169,7 @@ describe.each([{ client: anonymousClient, permission: 'No' }])(
         Types.ErrorStatusCode.MISSING_AUTHORIZATION_HEADER
       )
     })
+
     test(`${permission} key: try to reset ranking rules and be denied`, async () => {
       await expect(
         client.index(index.uid).resetRankingRules()
@@ -173,47 +181,43 @@ describe.each([{ client: anonymousClient, permission: 'No' }])(
   }
 )
 
-test(`Get request should not add double slash nor a trailing slash`, async () => {
-  try {
-    const res = await badHostClient.index(index.uid).getRankingRules()
-    expect(res).toBe(undefined) // Left here to trigger failed test if error is not thrown
-  } catch (e) {
-    expect(e.message).toMatch(
-      `${BAD_HOST}/indexes/movies_test/settings/ranking-rules`
+describe('Tests on url construction', () => {
+  test(`Test getRankingRules route`, async () => {
+    const route = `indexes/${index.uid}/settings/ranking-rules`
+    await expect(
+      badHostClient.index(index.uid).getRankingRules()
+    ).rejects.toHaveProperty(
+      'message',
+      `request to ${BAD_HOST}/${route} failed, reason: connect ECONNREFUSED ${BAD_HOST.replace(
+        'http://',
+        ''
+      )}`
     )
-    expect(e.message).not.toMatch(
-      `${BAD_HOST}/indexes/movies_test/settings/ranking-rules/`
-    )
-    expect(e.type).toBe('MeiliSearchCommunicationError')
-  }
-})
+  })
 
-test(`Update request should not add double slash nor a trailing slash`, async () => {
-  try {
-    const res = await badHostClient.index(index.uid).updateRankingRules([])
-    expect(res).toBe(undefined) // Left here to trigger failed test if error is not thrown
-  } catch (e) {
-    expect(e.message).toMatch(
-      `${BAD_HOST}/indexes/movies_test/settings/ranking-rules`
+  test(`Test updateRankingRules route`, async () => {
+    const route = `indexes/${index.uid}/settings/ranking-rules`
+    await expect(
+      badHostClient.index(index.uid).updateRankingRules([])
+    ).rejects.toHaveProperty(
+      'message',
+      `request to ${BAD_HOST}/${route} failed, reason: connect ECONNREFUSED ${BAD_HOST.replace(
+        'http://',
+        ''
+      )}`
     )
-    expect(e.message).not.toMatch(
-      `${BAD_HOST}/indexes/movies_test/settings/ranking-rules/`
-    )
-    expect(e.type).toBe('MeiliSearchCommunicationError')
-  }
-})
+  })
 
-test(`Reset request should not add double slash nor a trailing slash`, async () => {
-  try {
-    const res = await badHostClient.index(index.uid).resetRankingRules()
-    expect(res).toBe(undefined) // Left here to trigger failed test if error is not thrown
-  } catch (e) {
-    expect(e.message).toMatch(
-      `${BAD_HOST}/indexes/movies_test/settings/ranking-rules`
+  test(`Test resetRankingRules route`, async () => {
+    const route = `indexes/${index.uid}/settings/ranking-rules`
+    await expect(
+      badHostClient.index(index.uid).resetRankingRules()
+    ).rejects.toHaveProperty(
+      'message',
+      `request to ${BAD_HOST}/${route} failed, reason: connect ECONNREFUSED ${BAD_HOST.replace(
+        'http://',
+        ''
+      )}`
     )
-    expect(e.message).not.toMatch(
-      `${BAD_HOST}/indexes/movies_test/settings/ranking-rules/`
-    )
-    expect(e.type).toBe('MeiliSearchCommunicationError')
-  }
+  })
 })
