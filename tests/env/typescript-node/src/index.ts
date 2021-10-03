59777c5b2db1c5fda4c26ae74a8b2cd170121455
Rahul Singh <rahul.singh12221998@gmail.com>
2021-10-04 00:54:47 +0530

    lint fix

--- tests/env/typescript-node/src/index.ts
+++ tests/env/typescript-node/src/index.ts
@@ -18,39 +18,39 @@ interface Movie {
   genre?: string
 }
 
-const client = new MeiliSearch(config)
+const client = new MeiliSearch(config);
 
-  ; (async () => {
-    const index = await client.getOrCreateIndex<Movie>('movies')
+(async () => {
+  const index = await client.getOrCreateIndex<Movie>('movies')
 
-    const indexes = await client.getIndexes()
-    indexes.map((index: IndexResponse) => {
-      console.log(index.uid)
-      // console.log(index.something) -> ERROR
-    })
+  const indexes = await client.getIndexes()
+  indexes.map((index: IndexResponse) => {
+    console.log(index.uid)
+    // console.log(index.something) -> ERROR
+  })
 
-    const searchParams: SearchParams = {
-      limit: 5,
-      attributesToRetrieve: ['title', 'genre'],
-      attributesToHighlight: ['title'],
-      // test: true -> ERROR Test does not exist on type SearchParams
-    }
-    indexes.map((index: IndexResponse) => index.uid)
-    const res: SearchResponse<Movie> = await index.search(
-      'avenger',
-      searchParams
-    )
+  const searchParams: SearchParams = {
+    limit: 5,
+    attributesToRetrieve: ['title', 'genre'],
+    attributesToHighlight: ['title'],
+    // test: true -> ERROR Test does not exist on type SearchParams
+  }
+  indexes.map((index: IndexResponse) => index.uid)
+  const res: SearchResponse<Movie> = await index.search(
+    'avenger',
+    searchParams
+  )
 
-    // both work
-    const { hits }: { hits: Hits<Movie> } = res
+  // both work
+  const { hits }: { hits: Hits<Movie> } = res
 
-    hits.map((hit: Hit<Movie>) => {
-      console.log(hit?.genre)
-      console.log(hit.title)
-      // console.log(hit._formatted.title) -> ERROR, _formatted could be undefined
-      // console.log(hit?._formatted.title) -> ERROR, title could be undefined
-      console.log(hit?._formatted?.title)
-    })
+  hits.map((hit: Hit<Movie>) => {
+    console.log(hit?.genre)
+    console.log(hit.title)
+    // console.log(hit._formatted.title) -> ERROR, _formatted could be undefined
+    // console.log(hit?._formatted.title) -> ERROR, title could be undefined
+    console.log(hit?._formatted?.title)
+  })
 
-    await index.delete()
-  })()
+  await index.delete()
+})()
