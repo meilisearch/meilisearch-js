126998312d2f6d7a8d58a4281796becc96e800aa
Charlotte Vermandel <charlottevermandel@gmail.com>
2022-01-05 22:20:40 +0100

    Add headers tests

--- tests/env/express/tests/client.test.js
+++ tests/env/express/tests/client.test.js
@@ -1,19 +1,25 @@
 describe('MeiliSearch JS Browser test', () => {
   beforeAll(async () => {
-    await page.goto('http://localhost:3000')
+    await page.goto('http://localhost:3000/meilisearch')
   })
 
   it('Should have created an index and displayed it', async () => {
     await page.waitForSelector("#indexes")
-    await expect(
-      page.content()
-      ).resolves.toMatch('testIndex')
+    let element = await page.$('#indexes')
+    let value = await page.evaluate(el => el.textContent, element)
+    await expect(value).toMatch('testIndex')
+  })
+})
+
+describe('MeiliSearch JS Browser test', () => {
+  beforeAll(async () => {
+    await page.goto('http://localhost:3000/headers')
+  })
+  it('Should not throw cors error', async () => {
+    await page.waitForSelector("#error")
+    let element = await page.$('#error')
+    let value = await page.evaluate(el => el.textContent, element)
+    await expect(value).toMatch('NO ERRORS')
   })
 
-  it('Should have successfully searched on the index', async () => {
-    await page.waitForSelector("#indexes")
-    await expect(
-      page.content()
-      ).resolves.toMatch('wonder woman')
-  })
 })
