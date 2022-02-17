0001fe692adf2e2a0b4430d9895b25ce42afba0e
Charlotte Vermandel <charlottevermandel@gmail.com>
2022-02-17 13:02:37 -0500

    Remove redundant key test

--- tests/client_tests.ts
+++ tests/client_tests.ts
@@ -28,29 +28,6 @@ afterAll(() => {
   return clearAllIndexes(config)
 })
 
-describe.skip('Tests on client methods w/ master key', () => {
-  beforeEach(() => {
-    return clearAllIndexes(config)
-  })
-
-  test(`Master key: Should get keys`, async () => {
-    const client = new MeiliSearch({
-      ...config,
-      apiKey: MASTER_KEY,
-    })
-    const keys = await client.getKeys()
-
-    expect(keys).toHaveProperty(
-      'private',
-      '8dcbb482663333d0280fa9fedf0e0c16d52185cb67db494ce4cd34da32ce2092'
-    )
-    expect(keys).toHaveProperty(
-      'public',
-      '3b3bf839485f90453acc6159ba18fbed673ca88523093def11a9b4f4320e44a5'
-    )
-  })
-})
-
 describe.each([
   { permission: 'Master' },
   { permission: 'Private' },
