fd4b86fd6e851ccfc0020abe786372be7061eaf6
Charlotte Vermandel <charlottevermandel@gmail.com>
2022-08-03 16:04:49 +0200

    Remove unecessary tests

--- tests/search.test.ts
+++ tests/search.test.ts
@@ -130,12 +130,10 @@ describe.each([
     expect(response).toHaveProperty('hits', expect.any(Array))
     expect(response).toHaveProperty('offset', 0)
     expect(response).toHaveProperty('limit', 20)
-    expect(response).toHaveProperty('processingTimeMs', expect.any(Number))
-    expect(response).toHaveProperty('query', '"french book" about')
     expect(response.hits.length).toEqual(2)
   })
 
-  test(`${permission} key: Basic phrase searchwith optionalWords at none`, async () => {
+  test(`${permission} key: Basic phrase search with optionalWords at NONE`, async () => {
     const client = await getClient(permission)
     const response = await client
       .index(index.uid)
@@ -144,12 +142,10 @@ describe.each([
     expect(response).toHaveProperty('hits', expect.any(Array))
     expect(response).toHaveProperty('offset', 0)
     expect(response).toHaveProperty('limit', 20)
-    expect(response).toHaveProperty('processingTimeMs', expect.any(Number))
-    expect(response).toHaveProperty('query', '"french book" about')
     expect(response.hits.length).toEqual(1)
   })
 
-  test(`${permission} key: Basic phrase searchwith optionalWords at any`, async () => {
+  test(`${permission} key: Basic phrase search with optionalWords at ANY`, async () => {
     const client = await getClient(permission)
     const response = await client
       .index(index.uid)
@@ -158,8 +154,6 @@ describe.each([
     expect(response).toHaveProperty('hits', expect.any(Array))
     expect(response).toHaveProperty('offset', 0)
     expect(response).toHaveProperty('limit', 20)
-    expect(response).toHaveProperty('processingTimeMs', expect.any(Number))
-    expect(response).toHaveProperty('query', 'french book')
     expect(response.hits.length).toEqual(6)
   })
 
@@ -636,7 +630,7 @@ describe.each([{ permission: 'Master' }])(
       })
     })
 
-    test(`${permission} key: search with optional words to none`, async () => {
+    test(`${permission} key: search with optionalWords as NONE`, async () => {
       const client = await getClient(permission)
 
       const response = await client.index(index.uid).search('Another french', {
