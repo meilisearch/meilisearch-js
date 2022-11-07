613f1902bc31be1892169394fcbd7e2435d907d1
Charlotte Vermandel <charlottevermandel@gmail.com>
2022-11-07 17:04:46 +0100

    Add snapshot_creation task type for v0.30.0

--- src/types/types.ts
+++ src/types/types.ts
@@ -231,6 +231,7 @@ export const enum TaskTypes {
   DOCUMENTS_ADDITION_OR_UPDATE = 'documentAdditionOrUpdate',
   DOCUMENT_DELETION = 'documentDeletion',
   SETTINGS_UPDATE = 'settingsUpdate',
+  SNAPSHOT_CREATION = 'snapshotCreation',
 }
 
 export type TasksQuery = {
