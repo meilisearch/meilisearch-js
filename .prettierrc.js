73b619670cb68886926f5c26e775e3366faca2d6
F. Levi <55688616+flevi29@users.noreply.github.com>
2024-05-23 10:42:15 +0300

    Forgot about prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,14 +2,8 @@
 
 module.exports = {
   singleQuote: true,
-  // @TODO Remove as it's default
-  // arrowParens: 'always',
   semi: false,
-  // @TODO Remove as it's default
-  // bracketSpacing: true,
   trailingComma: 'es5',
-  // @TODO Remove as it's default
-  // printWidth: 80,
   plugins: ['./node_modules/prettier-plugin-jsdoc/dist/index.js'],
   // https://github.com/hosseinmd/prettier-plugin-jsdoc#tsdoc
   tsdoc: true,
