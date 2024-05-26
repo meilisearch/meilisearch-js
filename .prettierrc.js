de7f754a3029223f1c5d793fceec59544c430556
F. Levi <55688616+flevi29@users.noreply.github.com>
2024-05-26 09:51:23 +0300

    Remove prettier unnecessary custom options

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,6 @@
 // https://prettier.io/docs/en/options.html
 
 module.exports = {
-  singleQuote: true,
-  semi: false,
-  trailingComma: 'es5',
   plugins: ['./node_modules/prettier-plugin-jsdoc/dist/index.js'],
   // https://github.com/hosseinmd/prettier-plugin-jsdoc#tsdoc
   tsdoc: true,
