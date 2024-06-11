6dd472dd18d4e2743b6fcda62e2709d305d1bb55
curquiza <clementine@meilisearch.com>
2024-06-11 10:26:19 +0200

    Remove error and let threashold

--- src/types/types.ts
+++ src/types/types.ts
@@ -126,6 +126,7 @@ export type SearchParams = Query &
     showRankingScoreDetails?: boolean;
     attributesToSearchOn?: string[] | null;
     hybrid?: HybridSearch;
+    rankingScoreThreshold?: number;
   };
 
 // Search parameters for searches made with the GET method
@@ -1010,12 +1011,6 @@ export const ErrorStatusCode = {
 
   /** @see https://www.meilisearch.com/docs/reference/errors/error_codes#invalid_facet_search_facet_query */
   INVALID_FACET_SEARCH_FACET_QUERY: 'invalid_facet_search_facet_query',
-
-  /** @see https://www.meilisearch.com/docs/reference/errors/error_codes#invalid_search_ranking_score_threshold */
-  INVALID_SEARCH_RANKING_SCORE_THRESHOLD: 'invalid_search_ranking_score_threshold',
-
-  /** @see https://www.meilisearch.com/docs/reference/errors/error_codes#invalid_similar_ranking_score_threshold */
-  INVALID_SIMILAR_RANKING_SCORE_THRESHOLD: 'invalid_similar_ranking_score_threshold',
 };
 
 export type ErrorStatusCode =
