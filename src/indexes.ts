38cdcedb2c31fc7807389e9e5d16fe40b6188087
F. Levi <55688616+flevi29@users.noreply.github.com>
2024-08-20 10:52:40 +0300

    Fix style

--- src/indexes.ts
+++ src/indexes.ts
@@ -640,7 +640,6 @@ class Index<T extends Record<string, any> = Record<string, any>> {
    * https://www.meilisearch.com/docs/reference/api/experimental-features
    *
    * @param options - Object containing the function string and related options
-   * 
    * @returns Promise containing an EnqueuedTask
    */
   async updateDocumentsByFunction(
