56ffd44c8e7ff0101d2e73d4a3247976092ca88c
F. Levi <55688616+flevi29@users.noreply.github.com>
2024-08-21 09:27:06 +0300

    Include only soruce files in coverage

--- vitest.config.js
+++ vitest.config.js
@@ -2,8 +2,10 @@ const { defineConfig } = require('vitest/config');
 
 module.exports = defineConfig({
   test: {
-    include: ['tests/*.test.ts'],
+    include: 'tests/**/*.test.ts',
+    exclude: 'tests/env/**',
     fileParallelism: false,
     testTimeout: 100_000, // 100 seconds
+    coverage: { include: 'src/**/*.ts' },
   },
 });
