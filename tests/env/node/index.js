460e23c29cec8979a9262bc35c4378c474a98036
F. Levi <55688616+flevi29@users.noreply.github.com>
2024-10-04 21:07:08 +0300

    Fix env node test

--- tests/env/node/index.js
+++ tests/env/node/index.js
@@ -1,10 +1,10 @@
 const { MeiliSearch } = require('../../../')
 const DefaultMeiliSearch = require('../../../')
+const { generateTenantToken } = require('../../../dist/bundles/token.cjs')
 
 const CJStest = new MeiliSearch({ host:'http://localhost:7700', apiKey: 'masterKey'})
 const DefaultCJSTest = new DefaultMeiliSearch.MeiliSearch({ host:'http://localhost:7700', apiKey: 'masterKey'})
 
-DefaultCJSTest.generateTenantToken('e489fe16-3381-431b-bee3-00430192915d', []) // Resolved using the `main` field
-CJStest.generateTenantToken('e489fe16-3381-431b-bee3-00430192915d', []) // Resolved using the `main` field
+generateTenantToken('e489fe16-3381-431b-bee3-00430192915d', [])
 
 console.log({ CJStest, DefaultCJSTest })
