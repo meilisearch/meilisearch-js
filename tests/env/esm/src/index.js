805c88ba32c4da346710fb09e609f131578311ac
F. Levi <55688616+flevi29@users.noreply.github.com>
2024-10-05 18:43:55 +0300

    Fix env esm test

--- tests/env/esm/src/index.js
+++ tests/env/esm/src/index.js
@@ -1,8 +1,6 @@
-import { MeiliSearch } from '../../../../'
-import * as DefaultMeiliSearch from '../../../../'
-import { generateTenantToken } from '../../../../dist/bundles/token.mjs'
+import { MeiliSearch } from '../../../../dist/esm/index.js'
+import { generateTenantToken } from '../../../../dist/esm/token.js'
 
 const client = new MeiliSearch({ host:'http://localhost:7700', apiKey: 'masterKey' })
-const defaultClient = new DefaultMeiliSearch.MeiliSearch({ host:'http://localhost:7700', apiKey: 'masterKey' })
 const token = generateTenantToken('e489fe16-3381-431b-bee3-00430192915d', [], { apiKey: 'masterKey' })
-console.log({ client, token, defaultClient })
+console.log({ client, token })
--- tests/env/node/index.cjs
+++ tests/env/node/index.cjs
@@ -1,10 +1,8 @@
-const { MeiliSearch } = require('../../../')
-const DefaultMeiliSearch = require('../../../')
+const { MeiliSearch } = require('../../../dist/bundles/index.cjs')
 const { generateTenantToken } = require('../../../dist/bundles/token.cjs')
 
 const CJStest = new MeiliSearch({ host:'http://localhost:7700', apiKey: 'masterKey' })
-const DefaultCJSTest = new DefaultMeiliSearch.MeiliSearch({ host:'http://localhost:7700', apiKey: 'masterKey' })
 
 generateTenantToken('e489fe16-3381-431b-bee3-00430192915d', [], { apiKey: 'masterKey' })
 
-console.log({ CJStest, DefaultCJSTest })
+console.log({ CJStest })
