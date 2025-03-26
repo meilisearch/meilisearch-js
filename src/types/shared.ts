d8450508994720afc080eda59585028e0a8ce0a3
F. Levi <55688616+flevi29@users.noreply.github.com>
2025-03-26 15:41:32 +0200

    Fix lint issue

--- src/types/shared.ts
+++ src/types/shared.ts
@@ -1,7 +1,10 @@
+import type { RecordAny } from "./types.js";
+
 export type NonNullableDeepRecordValues<T> = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [P in keyof T]: T[P] extends any[]
     ? Array<NonNullableDeepRecordValues<T[P][number]>>
-    : T[P] extends Record<string, any>
+    : T[P] extends RecordAny
       ? NonNullableDeepRecordValues<T[P]>
       : NonNullable<T[P]>;
 };
