599d1befd43f402a45c7cfbf7835c61fc08f6384
Strift <lau.cazanove@gmail.com>
2025-04-01 14:34:15 +0800

    Use snapshots to keep track of batches structures

--- tests/batch.test.ts
+++ tests/batch.test.ts
@@ -33,15 +33,7 @@ describe.each([{ permission: "Master" }, { permission: "Admin" }])(
       const batches = await client.batches.getBatches();
       const batch = await client.batches.getBatch(batches.results[0].uid);
       expect(batch.uid).toEqual(batches.results[0].uid);
-      expect(batch.details).toBeDefined();
-      expect(batch.stats).toHaveProperty("totalNbTasks");
-      expect(batch.stats).toHaveProperty("status");
-      expect(batch.stats).toHaveProperty("types");
-      expect(batch.stats).toHaveProperty("indexUids");
-      expect(batch.duration).toBeDefined();
-      expect(batch.startedAt).toBeDefined();
-      expect(batch.finishedAt).toBeDefined();
-      expect(batch.progress).toBeDefined();
+      expect(batch).toMatchSnapshot();
     });
   },
 );
