a5dc5cffef2547d35b1bc55a3567ec188a02f8e0
Strift <lau.cazanove@gmail.com>
2025-04-01 15:16:37 +0800

    Remove snapshot testing for batches because too flaky

--- tests/batch.test.ts
+++ tests/batch.test.ts
@@ -33,7 +33,18 @@ describe.each([{ permission: "Master" }, { permission: "Admin" }])(
       const batches = await client.batches.getBatches();
       const batch = await client.batches.getBatch(batches.results[0].uid);
       expect(batch.uid).toEqual(batches.results[0].uid);
-      expect(batch).toMatchSnapshot();
+
+      // Can't use toMatchSnapshot because the output changes every time
+      expect(batch.details).toBeDefined();
+      expect(batch.stats).toHaveProperty("totalNbTasks");
+      expect(batch.stats).toHaveProperty("status");
+      expect(batch.stats).toHaveProperty("types");
+      expect(batch.stats).toHaveProperty("indexUids");
+      expect(batch.stats).toHaveProperty("progressTrace");
+      expect(batch.duration).toBeDefined();
+      expect(batch.startedAt).toBeDefined();
+      expect(batch.finishedAt).toBeDefined();
+      expect(batch.progress).toBeDefined();
     });
   },
 );
