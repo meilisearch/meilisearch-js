c0a56cda6c243c8fc6e5a7257dfa35de7499bf9a
F. Levi <55688616+flevi29@users.noreply.github.com>
2025-04-09 12:29:31 +0300

    Add test for index stats

---
+++ tests/index-stats.test.ts
@@ -0,0 +1,25 @@
+import { afterAll, expect, test } from "vitest";
+import { getClient } from "./utils/meilisearch-test-utils.js";
+
+const INDEX_UID = "stats-index";
+const client = await getClient("Master");
+
+afterAll(async () => {
+  await client.deleteIndex(INDEX_UID).waitTask();
+});
+
+test("getStats method", async () => {
+  await client.createIndex({ uid: INDEX_UID }).waitTask();
+  const stats = await client.index(INDEX_UID).getStats();
+  expect(stats).toMatchInlineSnapshot(`
+    {
+      "avgDocumentSize": 0,
+      "fieldDistribution": {},
+      "isIndexing": false,
+      "numberOfDocuments": 0,
+      "numberOfEmbeddedDocuments": 0,
+      "numberOfEmbeddings": 0,
+      "rawDocumentDbSize": 0,
+    }
+  `);
+});
