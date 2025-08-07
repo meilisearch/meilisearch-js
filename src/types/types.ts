a02f6f766d8c02b685616b49dfd1cf33e85bc27b
Strift <lau.cazanove@gmail.com>
2025-08-07 17:15:36 +0800

    Add chat settings API

--- src/types/types.ts
+++ src/types/types.ts
@@ -726,6 +726,20 @@ export type ChatCompletionRequest = {
   stream: boolean;
 };
 
+export type ChatSettings = {
+  description: string;
+  documentTemplate: string;
+  documentTemplateMaxBytes: number;
+  searchParameters: SearchParams;
+};
+
+export type ChatSettingsPayload = {
+  description?: string;
+  documentTemplate?: string;
+  documentTemplateMaxBytes?: number;
+  searchParameters?: Partial<SearchParams>;
+};
+
 /*
  ** Keys
  */
