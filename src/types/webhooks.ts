5824ac09f8f9f9ee4bccf548cc6c5b0bbf3a057a
Strift <lau.cazanove@gmail.com>
2025-08-13 14:26:49 +0800

    Add test to update webhook without changing url

--- src/types/webhooks.ts
+++ src/types/webhooks.ts
@@ -1,11 +1,20 @@
 export type Webhook = {
+  /** A v4 uuid Meilisearch automatically generates when you create a new webhook */
   uuid: string;
+  /** The URL Meilisearch should notify whenever it completes a task */
   url: string;
+  /** An object with HTTP headers and their values */
   headers?: Record<string, string>;
+  /**
+   * `true` for all webhooks created via the API and `false` for reserved
+   * webhooks
+   */
   isEditable: boolean;
 };
 
 export type WebhookPayload = {
+  /** The URL Meilisearch should notify whenever it completes a task */
   url?: string;
+  /** An object with HTTP headers and their values */
   headers?: Record<string, string>;
 };
