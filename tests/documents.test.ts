7f5924ad686e85e64d585ec5aaa401d0be575387
Maks <mixelburg@gmail.com>
2026-03-11 01:06:21 +0000

    style: fix prettier formatting in documents.test.ts

--- tests/documents.test.ts
+++ tests/documents.test.ts
@@ -748,7 +748,10 @@ describe("Documents tests", () => {
 
         await expect(
           client.index(indexPk.uid).getDocument(999),
-        ).rejects.toHaveProperty("cause.code", ErrorStatusCode.DOCUMENT_NOT_FOUND);
+        ).rejects.toHaveProperty(
+          "cause.code",
+          ErrorStatusCode.DOCUMENT_NOT_FOUND,
+        );
       });
 
       test(`${permission} key: Update documents with skipCreation:true does not create new documents`, async () => {
@@ -777,7 +780,10 @@ describe("Documents tests", () => {
 
         await expect(
           client.index(indexPk.uid).getDocument(999),
-        ).rejects.toHaveProperty("cause.code", ErrorStatusCode.DOCUMENT_NOT_FOUND);
+        ).rejects.toHaveProperty(
+          "cause.code",
+          ErrorStatusCode.DOCUMENT_NOT_FOUND,
+        );
       });
 
       test(`${permission} key: Add a document without a primary key and check response in task status`, async () => {
